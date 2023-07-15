c6a143f8db7f03838ad32f7218945b2f11f502b3
hodat2806 <hodat2806@gmail.com>
2023-07-15 16:46:28 +0700

    vvv

--- artisr/models.py
+++ artisr/models.py
@@ -5,9 +5,15 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
+    full_name = models.TextField(null=True)
     def __str__(self):
         return self.first_name
- 
+    
+    def save(self, *arg, **kwarg):
+        self.full_name = self.first_name + self.last_name
+        super().save(*arg, **kwarg)
+
+
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
